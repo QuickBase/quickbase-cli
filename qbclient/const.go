2ae64490f2e2ff1bf9a0428f4a97407517723824
cpliakas <cpliakas@gmail.com>
2021-01-20 17:02:05 -0500

    Removed unused code

--- qbclient/const.go
+++ qbclient/const.go
@@ -1,10 +1,5 @@
 package qbclient
 
-import (
-	"fmt"
-	"strings"
-)
-
 // Field* constants contain the Quick Base field types.
 const (
 	FieldRecordID           = "recordid"
@@ -40,83 +35,6 @@ const (
 	FieldvCard              = "vCardButton"
 	FieldPredecessor        = "predecessor"
 )
-
-// FieldType returns the Quick Base field type from in, which is a string that
-// contains the constant without the "Field" prefix.
-func FieldType(in string) (out string, err error) {
-	in = strings.ToLower(strings.ReplaceAll(in, "_", ""))
-
-	switch in {
-	case "recordid":
-		out = FieldRecordID
-	case "text":
-		out = FieldText
-	case "textmultiline":
-		out = FieldTextMultiLine
-	case "textmultiplechoice":
-		out = FieldTextMultipleChoice
-	case "richtext":
-		out = FieldRichText
-	case "multiselecttext", FieldMultiSelectText:
-		out = FieldMultiSelectText
-	case FieldNumeric:
-		out = FieldNumeric
-	case "numericcurrency", FieldNumericCurrency:
-		out = FieldNumericCurrency
-	case "numericpercent", FieldNumericPercent:
-		out = FieldNumericPercent
-	case "numericrating", FieldNumericRating:
-		out = FieldNumericRating
-	case "date":
-		out = FieldDate
-	case "datetime", FieldDateTime:
-		out = FieldDateTime
-	case "timeofday":
-		out = FieldTimeOfDay
-	case "duration":
-		out = FieldDuration
-	case "checkbox":
-		out = FieldCheckbox
-	case "address":
-		out = FieldAddress
-	case "addressstreet1":
-		out = FieldAddressStreet1
-	case "addressstreet2":
-		out = FieldAddressStreet2
-	case "addresscity":
-		out = FieldAddressCity
-	case "addressstateregion":
-		out = FieldAddressStateRegion
-	case "addresspostalcode":
-		out = FieldAddressPostalCode
-	case "addresscountry":
-		out = FieldAddressCountry
-	case "phonenumber", FieldPhoneNumber:
-		out = FieldPhoneNumber
-	case "emailaddress", FieldEmailAddress:
-		out = FieldEmailAddress
-	case "user", FieldUser:
-		out = FieldUser
-	case "userlist", FieldUserList:
-		out = FieldUserList
-	case "fileattachment", FieldFileAttachment:
-		out = FieldFileAttachment
-	case "url":
-		out = FieldURL
-	case "reportlink", FieldReportLink:
-		out = FieldReportLink
-	case "icalendar", FieldiCalendar:
-		out = FieldiCalendar
-	case "vcard", FieldvCard:
-		out = FieldvCard
-	case "predecessor":
-		out = FieldPredecessor
-	default:
-		err = fmt.Errorf("type not valid (%s)", in)
-	}
-
-	return
-}
 
 // AccumulationType* constants contain valid accumulation types for summary
 // fields.
